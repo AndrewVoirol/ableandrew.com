d79de8249e8203d40ad20415e25f1ed8353425ab
thesocialdiner <andrew@thesocialdiner.com>
2023-12-05 22:48:08 -0500

    commit

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://ableandrew.com/sitemap.xml',
+    host: 'https://ableandrew.com',
+  };
+}
